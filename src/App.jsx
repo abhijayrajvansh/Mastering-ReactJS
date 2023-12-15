7301c8a3d42b6037f4545c1d4191465632e82eb3
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-12-16 05:19:07 +0530

    update: added useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,29 @@ import './App.css'
 
 function App() {
 
-  let counter = 1
+  // let counter = 1 // javascript practice doesn't allow here to update variable value in UI
 
-  const addValue = () => {
-    counter = counter + 1
+  const [counter, setCounter] = useState(1) // const or let: useState hooks me fark nhi parta hai
+  // type [variableName, actionName] = useState.hook(defaultValue)
+
+  const increaseValue = () => {
+    setCounter(counter + 1)
     console.log("clicked", counter);
+  }
+
+  const decreaseValue = () => {
+    setCounter(counter - 1)
+    console.log("clicked", counter)
   }
 
   return (
     <>
       <h1>Mastering React.Js</h1>
-      <h2>counter value: {counter}</h2>
-      <button
-      onClick={addValue}
-      >Increase Value</button>
-      <button>Decrease Value</button>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={increaseValue} >Increase Value</button>
+      <button onClick={decreaseValue} >Decrease Value</button>
+      
       <p>footer: {counter}</p>
     </>
   )
