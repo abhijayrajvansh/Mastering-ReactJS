f24780bd070d7f2794a7c6533be6a3293a5d5072
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-12-17 06:53:48 +0530

    update: working on props

--- 02-tailwindAndProps/src/App.jsx
+++ 02-tailwindAndProps/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello world</h1>
+      <Card />
+      <Card />
     </>
   )
 }
